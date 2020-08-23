f37e43a38910a527e4f42460f225be8a6c7356d0
luke-robb-nz <57057115+luke-robb-nz@users.noreply.github.com>
2020-08-24 10:01:44 +1200

    some basic tidy up

--- geo_publish_logs.py
+++ geo_publish_logs.py
@@ -1,43 +1,50 @@
 import re
 import datetime as dt
 import pytimeparse
+import os
 
-log_file = r"C:/Users/luke.robb/OneDrive - Seequent/Desktop/geo_publish_logs_here/Maia_Geo_publish_#2.log"
+# log_file = r"C:/Users/luke.robb/OneDrive - Seequent/Desktop/geo_publish_logs_here/Maia_Geo_publish_#2.log"
 regex_start = r"START"
 regex_end = r"END"
 start_list = []
 end_list = []
 
 
-def read_log(log_file):
-    with open(log_file, 'r') as file:
-        for line in file:
-            if regex_start:
-                start_list.append(line)
-            if regex_end:
-                end_list.append(line)
+def get_publish_timeings(Publish_log_file):
+    if os.path.isfile(Publish_log_file):
+        with open(Publish_log_file, 'r') as file:
+            for line in file:
+                if regex_start:
+                    start_list.append(line)
+                if regex_end:
+                    end_list.append(line)
 
-            r = re.search(regex_start, line)
-            if r:
-                start = line.split(" ")[1].strip()
-                start = start.split(",")[0].strip()
-                print("Publish Start: ", start)
-                pstart = pytimeparse.parse(start)
+                r = re.search(regex_start, line)
+                if r:
+                    start = line.split(" ")[1].strip()
+                    start = start.split(",")[0].strip()
+                    print("Publish Start: ", start)
+                    publish_start = pytimeparse.parse(start)
 
-            r = re.search(regex_end, line)
-            if r:
-                end = line.split(" ")[1].strip()
-                end = end.split(",")[0].strip()
-                print("Publish End: ", end)
-                pend = pytimeparse.parse(end)
+                r = re.search(regex_end, line)
+                if r:
+                    end = line.split(" ")[1].strip()
+                    end = end.split(",")[0].strip()
+                    print("Publish End: ", end)
+                    publish_end = pytimeparse.parse(end)
 
-                duration_seconds = pend - pstart
-                duration = dt.timedelta(seconds=duration_seconds)
+                    duration_seconds = publish_end - publish_start
+                    duration = dt.timedelta(seconds=duration_seconds)
 
-                print("Publish Duration:", duration)
+                    print("Publish Duration:", duration)
+                    break
+    else:
+        print(f"File not found, is {Publish_log_file} the correct path")
 
 
-read_log(log_file)
+def get_download_timeings():
+    pass
 
 
-
+if __name__ == "__main__":
+    get_publish_timeings(r"C:/Users/luke.robb/OneDrive - Seequent/Desktop/geo_publish_logs_here/Maia_Geo_publish_#2.log")
