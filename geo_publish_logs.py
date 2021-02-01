b630d880ccc87ec245c1234c283609ccc37839d2
luke-robb-nz <57057115+luke-robb-nz@users.noreply.github.com>
2021-02-01 15:22:15 +1300

    added download timings

--- geo_publish_logs.py
+++ geo_publish_logs.py
@@ -3,8 +3,8 @@ import datetime as dt
 import pytimeparse
 import os
 
-regex_start = r"START"
-regex_end = r"END"
+regex_start = r"-- PUBLISH START --"
+regex_end = r"PUBLISH END"
 start_list = []
 end_list = []
 
@@ -12,6 +12,49 @@ regex_download_start = r"about to download"
 regex_download_end = r"files unpacked"
 download_start_list = []
 download_end_list = []
+#this is the default location geo wrights its logs to, plan being to cut out the manual transfer
+
+start_timings = []
+end_timings = []
+
+
+def get_latest_publish_timings(default_log_location):
+    with open(default_log_location, 'r') as file:
+        for line in file:
+            if regex_start:
+                start_timings.append(line)
+            r = re.search(regex_start, line)
+            if r:
+                start_timings.append(line)
+                last_start = start_timings[-1]
+                publish_start = pytimeparse.parse(last_start)
+
+            r = re.search(regex_end, line)
+            if r:
+                end_timings.append(line)
+                last_end = end_timings[-1]
+                publish_end = pytimeparse.parse(last_end)
+
+
+        last_start.split(" ")[1].strip()
+        last_start = last_start.split(",")[0].strip()
+        print("Publish Start: ", publish_start)
+
+        last_end.split(" ")[1].strip()
+        last_end = last_end.split(",")[0].strip()
+        print("Publish End: ", publish_end)
+
+
+        duration_seconds = publish_end - publish_start
+        duration = dt.timedelta(seconds=duration_seconds)
+
+        print(f"Publish Duration: {duration}\n--------------------------------------------------------------")
+
+
+
+
+
+
 
 
 def get_publish_timings(publish_log_file):
@@ -20,7 +63,7 @@ def get_publish_timings(publish_log_file):
             for line in file:
                 if regex_start:
                     start_list.append(line)
-                if regex_download_end:
+                if regex_end:
                     end_list.append(line)
 
                 r = re.search(regex_start, line)
@@ -79,6 +122,7 @@ def get_download_timings(download_log_file):
 
 
 if __name__ == "__main__":
-    get_publish_timings(r"C:/Users/luke.robb/OneDrive - Seequent/Desktop/geo_publish_logs_here/Maia_Geo_publish_#2.log")
-    get_download_timings(r"C:\Users\luke.robb\OneDrive - Seequent\Desktop\geo_download_logs/maia_geo_download.log")
+    get_publish_timings(r"C:/Users/luke.robb/OneDrive - Seequent/Desktop/geo_publish_logs_here/sigma.log")
+    get_download_timings(r"C:\Users\luke.robb\OneDrive - Seequent\Desktop\geo_download_logs/")
+#    get_latest_publish_timings(r"C:\Users\LUKE~1.ROB\AppData\Local\Temp\Leapfrog Geo logs\Geo.log")
 
